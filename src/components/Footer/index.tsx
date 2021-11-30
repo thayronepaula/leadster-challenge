8a55d6e8182402078dc409401f4bbfef6ce84d1a
thayrone <thayronedepaula15@gmail.com>
2021-11-30 11:32:35 -0300

    style: add year dynamic

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,19 @@
 import styles from "./styles.module.scss";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
-      <p>Direitos reservados à Pexels</p>
+      <p>
+        Direitos reservados à
+        <a href="https://www.pexels.com/" target="_blank">
+          Pexels
+        </a>
+      </p>
+      <hr />
+
+      <span>{currentYear}</span>
     </footer>
   );
 }
